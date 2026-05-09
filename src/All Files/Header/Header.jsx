1f806742ca566b06922c75b4537e7b32fad24c25
Zeeshan Ali Zafar <zeeshanalizafar53@gmail.com>
2026-05-09 13:00:55 +0500

    New

--- src/All Files/Header/Header.jsx
+++ src/All Files/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
 
 
   <div className="hero">
-    <h1>Sales Performance Dashboard</h1>
+    <h1 className='font-bold'>Sales Dashboard</h1>
     <input type="text" className="search-bar" placeholder="Search sales, revenue, date..."/>
   </div>
 
